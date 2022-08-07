e7d854973ee6220dccf6fc15d6b253cf01ce8b73
Max Schulte <maximilliangschulte@pm.me>
2022-08-07 17:48:12 -0500

    style

--- src/App.svelte
+++ src/App.svelte
@@ -37,30 +37,32 @@
 
 <div id="app">
 	<div id="toggle" class="proxytype-{proxyType}" on:click={handleClick}>
-    Toggle Manual Network Configuration: {proxyType === "manual" ? "ON" : "OFF"}
+      Toggle Manual Network Configuration: {proxyType === "manual" ? "ON" : "OFF"}
 	</div>
 </div>
 
 <style>
 	#app {
-		display: grid;
 		grid-template-rows: 20px auto;
 		bottom: 0;
 		left: 0;
 		position: absolute;
 		right: 0;
 		top: 0;
-		font-size: 1rem;
-		margin: 0;
-		height: 400px;
-		width: 400px;
-		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-			Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 	}
+  #toggle {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80%;
+    width: 80%;
+    margin: 10%;
+  }
 	.proxytype-none {
-		background-color: red;
+		background-color: lightgray;
 	}
 	.proxytype-manual {
 		background-color: purple;
+    color: white;
 	}
 </style>
