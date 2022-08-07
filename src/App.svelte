4cd621c3a2d491ae951ba9869d49dcb8607880ef
Max Schulte <maximilliangschulte@pm.me>
2022-08-07 17:18:17 -0500

    basic toggling of network configuration to manual and none

--- src/App.svelte
+++ src/App.svelte
@@ -1,59 +1,66 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import browser from "webextension-polyfill";
+	import { onMount } from "svelte";
+	import browser from "webextension-polyfill";
 
 
-  let proxyType = "none";
-  function getProxySettings(){
-    let proxySettings;
-    browser.proxy.settings.get({}).then((x) => {
-      proxySettings = x;
-    });
-    return proxySettings;
-  }
-  onMount(async () => {
-    let proxySettings = getProxySettings();
-    proxyType = proxySettings.proxyType || "none";
-  });
-  function handleClick(e){
-	console.log(e)
-  	if (proxyType === "none") {
-		proxyType = "manual";
-		return;
-	}
-	proxyType = "none";
+	let proxyType = "none";
+  function setNested(obj, keys: Array<string>, value) {
+    const key = keys[0];
+    if (keys.length == 1)
+      return {...obj, [key]: value};
+    const nested = setNested(obj[key], keys.slice(1), value);
+    return {...obj, [key]: nested};
   };
+
+	onMount(async () => {
+    browser.proxy.settings.get({}).then((proxySettings)=>{
+      proxyType = proxySettings.value.proxyType || "none";
+    }).catch((err)=>{
+      alert(`getting proxy settings on mount: ${err}`);
+    });
+	});
+
+	function handleClick(e){
+    browser.proxy.settings.get({}).then((proxySettings)=>{
+      const proxyTypeNew = (proxySettings.value.proxyType === "none") ? "manual" : "none";
+      const proxySettingsNew = setNested(proxySettings, ["value", "proxyType"], proxyTypeNew);
+      browser.proxy.settings.set({value: proxySettingsNew.value}).then(()=>{
+        proxyType = proxyTypeNew;
+      }).catch((err)=>{
+        alert(`setting proxy settings: ${err}`);
+      });
+    }).catch((err)=>{
+      alert(`getting proxy settings: ${err}`);
+    });
+	};
 </script>
 
 <div id="app">
-	<button id="toggle" class="proxytype-{proxyType}" on:click={handleClick}>
-		hello
-	</button>
+	<div id="toggle" class="proxytype-{proxyType}" on:click={handleClick}>
+    Toggle Manual Network Configuration: {proxyType === "manual" ? "ON" : "OFF"}
+	</div>
 </div>
 
 <style>
-  #app {
-    display: grid;
-    grid-template-rows: 20px auto;
-    bottom: 0;
-    left: 0;
-    position: absolute;
-    right: 0;
-    top: 0;
-    font-size: 1rem;
-    margin: 0;
-    height: 400px;
-    width: 400px;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  }
-  #toggle {
-  	
-  }
-  .proxytype-none {
-	  background-color: none,
-  }
-  .proxytype-manual {
-	  background-color: purple,
-  }
+	#app {
+		display: grid;
+		grid-template-rows: 20px auto;
+		bottom: 0;
+		left: 0;
+		position: absolute;
+		right: 0;
+		top: 0;
+		font-size: 1rem;
+		margin: 0;
+		height: 400px;
+		width: 400px;
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+			Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+	}
+	.proxytype-none {
+		background-color: red;
+	}
+	.proxytype-manual {
+		background-color: purple;
+	}
 </style>
